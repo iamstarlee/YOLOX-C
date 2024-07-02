6b981fd488f6646985d9b7e107d5ec5c3803100b
iamstarlee <13253620092@163.com>
2024-07-02 16:08:47 +0800

    update these files

---
+++ build/src/CMakeFiles/ort_utility.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ort_utility.dir/ImageRecognitionOrtSessionHandlerBase.cpp.o"
+  "CMakeFiles/ort_utility.dir/ImageRecognitionOrtSessionHandlerBase.cpp.o.d"
+  "libort_utility.pdb"
+  "libort_utility.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ort_utility.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/CMakeFiles/ort_utility.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ort_utility.
+# This may be replaced when dependencies are built.
